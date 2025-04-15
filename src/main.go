9d5d5e284d7fba11a226415f0c20597c4555f29d
jusimen <jusi.monteiro@protonmail.com>
2025-04-15 15:59:34 +0100

    Refactor main.go to improve GTFS validation flow
    - Moved validation logic into a separate function `runValidations` for better organization and readability.
    - Enhanced the main function to include file validation checks, logging GTFS file names, and handling validation errors more effectively.
    - Updated the GTFS zip file path for testing purposes and ensured proper error handling during file reading.
    - Improved overall structure and clarity of the main application logic, facilitating easier maintenance and future enhancements.

--- src/main.go
+++ src/main.go
@@ -4,26 +4,14 @@ import (
 	"log"
 	"main/src/lib"
 	"main/src/services"
+	"main/src/types"
 	"main/src/validations"
+	file_validation "main/src/validations/files"
+	"strings"
 	"sync"
 )
 
-func main() {
-
-	// Clear the terminal
-	lib.AppLogger.Clear()
-	lib.AppLogger.Divider("GTFS Validator")
-	tracker := lib.AppLogger.StartPerformanceTracker("Reading GTFS")
-
-	// gtfs, err := services.ReadGTFSZip("data/CMET.zip")
-	gtfs, err := services.ReadGTFSZip("data/bad-format.zip")
-	if err != nil {
-		log.Fatalf("Error reading GTFS: %v", err)
-	}
-
-	tracker.End()
-	tracker = lib.AppLogger.StartPerformanceTracker("Running Validations")
-
+func runValidations(gtfs types.Gtfs, tracker *lib.PerformanceTracker) {
 	// Create a wait group to wait for all validations to complete
 	var wg sync.WaitGroup
 
@@ -45,8 +33,40 @@ func main() {
 	// Wait for all validations to complete
 	wg.Wait()
 	tracker.End()
+}
 
-	// Print Summary
-	// services.AppMessageService.PrintSummary()
+func main() {
+
+	// Clear the terminal
+	lib.AppLogger.Clear()
+	lib.AppLogger.Divider("GTFS Validator")
+
+	// Start Performance Tracker
+	tracker := lib.AppLogger.StartPerformanceTracker("Reading GTFS")
+
+	// Read GTFS from zip file
+	// gtfs, err := services.ReadGTFSZip("data/CMET.zip")
+	gtfs, err := services.ReadGTFSZip("data/bad-files.zip")
+	if err != nil {
+		log.Fatalf("Error reading GTFS: %v", err)
+	}
+	// Check File Requirements
+	fileNames := make([]string, 0, len(gtfs.Files))
+	for name := range gtfs.Files {
+		fileNames = append(fileNames, name)
+	}
+	lib.AppLogger.Accent("GTFS Files: ", strings.Join(fileNames, ", "))
+	if errs := file_validation.NewFileValidation(nil).Validate(gtfs); len(errs) > 0 {
+		lib.AppLogger.Error("Errors found in file requirements")
+		for _, err := range errs {
+			lib.AppLogger.Error(err.Message)
+		}
+		panic("Errors found in file requirements, aborting...")
+	}
+
+	// Run Validations for each file
+	runValidations(gtfs, tracker)
+
+	// Print Table
 	services.AppMessageService.PrintTable()
 }
