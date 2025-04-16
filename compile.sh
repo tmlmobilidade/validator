cf3951cc6161bc08b63a071fa9074eefe3fd67e7
jusimen <jusi.monteiro@protonmail.com>
2025-04-16 15:24:13 +0100

    Update compile script to reference main.go directly and adjust output paths for compiled binaries
    - Modified the compile.sh script to change the output paths for the compiled binaries, ensuring they are placed in the correct directory.
    - Updated the build commands to reference the main.go file directly, improving the script's clarity and maintainability.

--- compile.sh
+++ compile.sh
@@ -5,22 +5,22 @@ if [ ! -d "bin" ]; then
     mkdir bin
 fi
 
-
+cd validator
 
 # Compile the validator for linux
-GOOS=linux GOARCH=amd64 go build -o bin/validator-linux-amd64 validator/main.go
+GOOS=linux GOARCH=amd64 go build -o ../bin/validator-linux-amd64 ./main.go
 
 # Compile the validator for linux arm64
-GOOS=linux GOARCH=arm64 go build -o bin/validator-linux-arm64 validator/main.go
+GOOS=linux GOARCH=arm64 go build -o ../bin/validator-linux-arm64 ./main.go
 
 # Compile the validator for darwin x86_64
-GOOS=darwin GOARCH=amd64 go build -o bin/validator-darwin-amd64 validator/main.go
+GOOS=darwin GOARCH=amd64 go build -o ../bin/validator-darwin-amd64 ./main.go
 
 # Compile the validator for darwin arm64
-GOOS=darwin GOARCH=arm64 go build -o bin/validator-darwin-arm64 validator/main.go
+GOOS=darwin GOARCH=arm64 go build -o ../bin/validator-darwin-arm64 ./main.go
 
 # Compile the validator for windows x86_64
-GOOS=windows GOARCH=amd64 go build -o bin/validator.exe validator/main.go
+GOOS=windows GOARCH=amd64 go build -o ../bin/validator.exe ./main.go
 
 
 # Check if the compilation was successful
@@ -28,3 +28,5 @@ if [ $? -ne 0 ]; then
     echo "Error: Failed to compile the validator"
     exit 1
 fi
+
+cd ..
