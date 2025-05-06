068d420e1209e99a7e23e366987ffa9a3ebce018
jusimen <jusi.monteiro@protonmail.com>
2025-05-06 16:59:45 +0100

    Add route ID validation function for trip data

---
+++ validator/validations/trips/route_id_validation.go
@@ -0,0 +1,43 @@
+package trips
+
+import (
+	"main/services"
+	"main/types"
+)
+
+/*
+# Attributes
+
+	- File: [trips.txt]
+	- Field: route_id
+	- Presence: Required
+	- Type: Foreign Key referencing routes.route_id
+
+# Description
+
+Identifies a route.
+
+[trips.txt]: https://gtfs.org/schedule/reference/#trips
+*/
+func RouteIdValidation(trip *types.Trip, row int, gtfs *types.Gtfs) {
+	message := types.Message{
+		Field: "route_id",
+		FileName: "trips.txt",
+		Message: "Route ID is required",
+		Rows: []int{row},
+		Severity: types.SEVERITY_ERROR,
+		ValidationID: "route_id_validation",
+	}
+
+	if trip.RouteId == "" {
+		message.Message = "Route ID is required"
+		services.AppMessageService.AddMessage(message)
+		return;
+	}
+
+	// Check if route_id is Foreign Key referencing routes.route_id
+	if _, ok := gtfs.IdMap["routes"][trip.RouteId]; !ok {
+		message.Message = "Route ID must reference a valid route_id from routes.txt."
+		services.AppMessageService.AddMessage(message)
+	}
+}
