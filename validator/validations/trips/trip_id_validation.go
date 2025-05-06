48b92a0a9e66f38eb2267200edcb0bdd5fc007d3
jusimen <jusi.monteiro@protonmail.com>
2025-05-06 17:00:11 +0100

    Add Trip ID validation function for trip data

---
+++ validator/validations/trips/trip_id_validation.go
@@ -0,0 +1,54 @@
+package trips
+
+import (
+	"main/services"
+	"main/types"
+)
+
+/*
+# Attributes
+
+	- File: [trips.txt]
+	- Field: trip_id
+	- Presence: Required
+	- Type: Unique ID
+
+# Description
+
+Identifies a trip.
+
+[trips.txt]: https://gtfs.org/schedule/reference/#trips
+*/
+func TripIdValidation(trip *types.Trip, row int, gtfs *types.Gtfs) {
+
+	message := types.Message{
+		Field: "trip_id",
+		FileName: "trips.txt",
+		Message: "Trip ID is required",
+		Rows: []int{row},
+		Severity: types.SEVERITY_ERROR,
+		ValidationID: "trip_id_validation",
+	}
+
+	//  Check if trip_id is required
+	if trip.TripId == "" && len(gtfs.Files["trips"]) > 1 {
+		message.Message = "Trip ID is required when there is more than one trip"
+		message.Severity = types.SEVERITY_ERROR
+	}
+
+	if trip.TripId != "" {
+		// Check if trip_id is Unique ID
+		if _, ok := gtfs.IdMap["trips"][trip.TripId]; ok && row != gtfs.IdMap["trips"][trip.TripId] {
+			services.AppMessageService.AddMessage(types.Message{
+				Field: "trip_id",
+				FileName: "trips.txt",
+				ValidationID: "duplicate_trips_validation",
+				Message: "Duplicate trip_id found. Trip IDs must be unique.",
+				Rows: []int{row},
+				Severity: types.SEVERITY_ERROR,
+			})
+		}
+	}
+
+	services.AppMessageService.AddMessage(message)
+}
