e63880a2036882381d32c0c0d17c619ef72fbe8d
jusimen <jusi.monteiro@protonmail.com>
2025-05-23 17:56:38 +0100

    Add route_desc validation logic and unit tests

---
+++ validator/validations/routes/validations/route_desc_validation.go
@@ -0,0 +1,62 @@
+package routes
+
+import (
+	"main/lib"
+	"main/services"
+	"main/types"
+)
+
+/*
+# Attributes
+
+- File: [routes.txt]
+- Field: route_desc
+- Presence: Optional
+- Type: String
+
+# Description
+
+Description of a route that provides useful, quality information. Should not be a duplicate of route_short_name or route_long_name.
+
+# Example
+
+"A" trains operate between Inwood-207 St, Manhattan and Far Rockaway-Mott Avenue, Queens at all times. Also from about 6AM until about midnight, additional "A" trains operate between Inwood-207 St and Lefferts Boulevard (trains typically alternate between Lefferts Blvd and Far Rockaway).
+
+Conditionally Required:
+    - Required if routes.route_short_name is empty.
+    - Optional otherwise.
+
+[routes.txt]: https://gtfs.org/schedule/reference/#routestxt
+*/
+func RouteDescValidation(severity *types.Severity, route *types.Route, row int) {
+	s := types.SEVERITY_IGNORE
+	if severity != nil {
+		s = *severity
+	}
+
+	addMessage := func(msg string, severity types.Severity) {
+		services.AppMessageService.AddMessage(types.Message{
+			Field:        "route_desc",
+			FileName:     "routes.txt",
+			ValidationID: "route_desc_validation",
+			Message:      msg,
+			Rows:         []int{row},
+			Severity:     severity,
+		})
+	}
+
+	if route.RouteDesc == nil || *route.RouteDesc == "" {
+		if s != types.SEVERITY_IGNORE {
+			warn := lib.IfThenElse(s == types.SEVERITY_WARNING, "route_desc is recommended.", "route_desc is required.")
+			addMessage(warn, s)
+			return
+		}
+	}
+
+	if route.RouteShortName != nil && *route.RouteDesc == *route.RouteShortName {
+		addMessage("route_desc should not duplicate route_short_name.", types.SEVERITY_WARNING)
+	}
+	if route.RouteLongName != nil && *route.RouteDesc == *route.RouteLongName {
+		addMessage("route_desc should not duplicate route_long_name.", types.SEVERITY_WARNING)
+	}
+} 
