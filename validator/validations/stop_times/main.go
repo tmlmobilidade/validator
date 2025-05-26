11f7cd982e4eaa22aff941f1584bfad081cab50b
jusimen <jusi.monteiro@protonmail.com>
2025-05-26 15:13:34 +0100

    Add stop_sequence validation logic and corresponding unit tests for stop times. Implement checks for required presence, non-negative values, and increasing order, enhancing the robustness of GTFS validation.

--- validator/validations/stop_times/main.go
+++ validator/validations/stop_times/main.go
@@ -19,16 +19,22 @@ func RunValidations(gtfs types.Gtfs) {
 		// Validate trip_id
 		validations.TripIdValidation(&stopTime, i, &gtfs)
 
+		// Validate stop_sequence
+		validations.StopSequenceValidation(&stopTime, i, &gtfs)
+		
+		// Validate arrival_time
+		validations.ArrivalTimeValidation(nil, &stopTime, i, &gtfs)
+		
+		// Validate departure_time
+		validations.DepartureTimeValidation(nil, &stopTime, i, &gtfs)
+		
 		// Validate stop_id
 		validations.StopIdValidation(&stopTime, i, &gtfs)
-
+		
 		// Validate location_group_id
 		validations.LocationGroupIdValidation(&stopTime, i, &gtfs)
 
-		// Validate arrival_time
-		validations.ArrivalTimeValidation(nil, &stopTime, i, &gtfs)
-
-		// Validate departure_time
-		validations.DepartureTimeValidation(nil, &stopTime, i, &gtfs)
+		// TODO: Validate location_id
+		// validations.LocationIdValidation(&stopTime, i, &gtfs)
 	}
 }
