ad9f111a35dad8a29f2fa60eef917b98cc0fca94
jusimen <jusi.monteiro@protonmail.com>
2025-07-04 19:23:21 +0100

    refactor: update stop_url_validation to make stop_url presence optional and enhance error messaging based on severity rules

--- validator/validations/stops/validations/stop_url_validation.go
+++ validator/validations/stops/validations/stop_url_validation.go
@@ -13,7 +13,7 @@ import (
 
   - File: [stop.txt]
   - Field: stop_url
-  - Presence: Required
+  - Presence: Optional
   - Type: URL
 
 # Description
@@ -24,25 +24,35 @@ URL of the transit stop.
 */
 func StopUrlValidation(stop *types.Stop, row int, rules *types.StopsRules) {
 
-	addMessage := func(message string) {
+	s := types.SEVERITY_IGNORE
+	if rules != nil {
+		s = rules.StopUrl.Severity
+	}
+
+	addMessage := func(message string, severity types.Severity) {
 		services.AppMessageService.AddMessage(types.Message{
 			Field:        "stop_url",
 			FileName:     "stop.txt",
 			Message:      message,
 			Rows:         []int{row},
-			Severity:     types.SEVERITY_ERROR,
+			Severity:     severity,
 			ValidationID: "stop_url_validation",
 		})
 	}
 
 	if stop.StopUrl == nil {
-		addMessage("Stop URL is required")
+		if s == types.SEVERITY_IGNORE {
+			return
+		}
+
+		warn := lib.IfThenElse(s == types.SEVERITY_ERROR, "stop_url is required", "stop_url is recommended")
+		addMessage(warn, s)
 		return
 	}
 
 	err := lib.ValidateUrl(*stop.StopUrl)
 	if err != "" {
-		addMessage(err)
+		addMessage(err, types.SEVERITY_ERROR)
 		return
 	}
 
@@ -53,7 +63,7 @@ func StopUrlValidation(stop *types.Stop, row int, rules *types.StopsRules) {
 		}
 
 		if !slices.Contains(*rules.StopUrl.Options, *stop.StopUrl) {
-			addMessage(fmt.Sprintf("stop_url is not allowed: %s", *stop.StopUrl))
+			addMessage(fmt.Sprintf("stop_url is not allowed: %s", *stop.StopUrl), s)
 			return
 		}
 	}
