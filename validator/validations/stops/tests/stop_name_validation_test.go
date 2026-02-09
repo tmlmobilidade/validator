6e468caa3a3ad26d4437b1650d31b4e5640bbb3a
LoudNeo <meliodasgamer077@gmail.com>
2026-02-09 12:28:11 +0000

    refactor stop name validation logic and tests to enhance error handling; update location type references and streamline test case names for improved clarity

--- validator/validations/stops/tests/stop_name_validation_test.go
+++ validator/validations/stops/tests/stop_name_validation_test.go
@@ -10,19 +10,20 @@ import (
 )
 
 func TestAllStopNameValidationTestCases(t *testing.T) {
-	locationTypeValidOptions := test_helpers.GetLocationTypeValidOptions()
-
 	for _, tc := range test_helpers.GetGenericRequiredFieldTestCases("stop_name") {
+		if tc.Name == "Recommended_Missing" {
+			continue
+		}
 		t.Run(tc.Name, func(t *testing.T) {
 			services.AppMessageService.Clear()
 
-			stop := &types.Stop{StopName: tc.Value, LocationType: &locationTypeValidOptions[0]}
+			stop := &types.Stop{StopName: tc.Value, LocationType: lib.Ptr(0)}
 
 			if tc.Name == "Required" {
-				stop = &types.Stop{StopName: nil, LocationType: &locationTypeValidOptions[0]}
+				stop = &types.Stop{StopName: nil, LocationType: lib.Ptr(0)}
 			}
 			if tc.Name == "Invalid_Value" {
-				stop = &types.Stop{StopName: lib.Ptr(""), LocationType: &locationTypeValidOptions[0]}
+				stop = &types.Stop{StopName: lib.Ptr(""), LocationType: lib.Ptr(0)}
 			}
 
 			validations.StopNameValidation(stop, tc.Row, nil)
@@ -31,23 +32,22 @@ func TestAllStopNameValidationTestCases(t *testing.T) {
 	}
 
 	for _, tc := range test_helpers.GetGenericSeverityTestCases("stop_name") {
-		if tc.Name == "Severity_Forbidden_Missing" {
+		if tc.Name == "Severity_Warning_Missing" {
 			continue
 		}
 		t.Run(tc.Name, func(t *testing.T) {
 			services.AppMessageService.Clear()
-			stop := &types.Stop{StopName: nil, LocationType: &locationTypeValidOptions[0]}
+			stop := &types.Stop{StopName: nil, LocationType: lib.Ptr(0)}
 			validations.StopNameValidation(stop, tc.Row, &types.StopsRules{StopName: types.RuleConfig{Severity: tc.Severity}})
-			test_helpers.AssertMessageCount(t, services.AppMessageService, tc.ExpectedErrors, tc.Name, types.SEVERITY_ERROR)
-			test_helpers.AssertMessageCount(t, services.AppMessageService, tc.ExpectedWarnings, tc.Name, types.SEVERITY_WARNING)
+			test_helpers.AssertMessageCount(t, services.AppMessageService, tc.ExpectedErrors, tc.Name, tc.Severity)
 		})
 	}
 
-	t.Run("Optional_LocationType3_Missing", func(t *testing.T) {
+	t.Run("Optional_LocationType3", func(t *testing.T) {
 		services.AppMessageService.Clear()
-		stop := &types.Stop{StopName: nil, LocationType: &locationTypeValidOptions[3]}
+		stop := &types.Stop{StopName: nil, LocationType: lib.Ptr(3)}
 		validations.StopNameValidation(stop, 2, nil)
-		test_helpers.AssertMessageCount(t, services.AppMessageService, 0, "Optional_LocationType3_Missing", types.SEVERITY_ERROR)
+		test_helpers.AssertMessageCount(t, services.AppMessageService, 0, "Optional_LocationType3", types.SEVERITY_ERROR)
 	})
 
 }
