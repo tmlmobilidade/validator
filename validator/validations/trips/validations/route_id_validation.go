d15eae7652355ea75de38ef8fa94d267b7a08dea
jusimen <jusi.monteiro@protonmail.com>
2026-02-10 11:37:42 +0000

    remove commented-out code in route_id_validation and service_id_validation functions to improve code clarity and maintainability

--- validator/validations/trips/validations/route_id_validation.go
+++ validator/validations/trips/validations/route_id_validation.go
@@ -33,9 +33,4 @@ func RouteIdValidation(trip *types.Trip, row int, gtfs *types.Gtfs) {
 	if err != nil || len(rows) == 0 {
 		ctx.AddError(ctx.GetTranslatedMessage("route_id_validation.not_found", map[string]interface{}{"route_id": *trip.RouteId}))
 	}
-
-	// Check if route_id is Foreign Key referencing routes.route_id
-	// if !lib.GtfsIdMapKeyExists(gtfs, "routes", *trip.RouteId) {
-	// 	ctx.AddError(ctx.GetTranslatedMessage("route_id_validation.not_found", map[string]interface{}{"route_id": *trip.RouteId}))
-	// }
 }
