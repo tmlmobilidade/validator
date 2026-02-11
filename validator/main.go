9a763ecb06a72d05eebfcd348d4979855a892971
LoudNeo <meliodasgamer077@gmail.com>
2026-02-11 15:00:51 +0000

    Remove unused fare media validation import from main.go to streamline the validation process.

--- validator/main.go
+++ validator/main.go
@@ -25,7 +25,6 @@ import (
 	_ "main/validations/fare_attributes"
 	_ "main/validations/fare_leg_join_rules"
 	_ "main/validations/fare_leg_rules"
-	_ "main/validations/fare_media"
 	_ "main/validations/fare_products"
 	_ "main/validations/fare_rules"
 	_ "main/validations/fare_transfer_rules"
