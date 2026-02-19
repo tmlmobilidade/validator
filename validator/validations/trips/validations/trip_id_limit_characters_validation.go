8c818faf8a09842cc88b091f68a07b2f4c1936ab
jusimen <jusi.monteiro@protonmail.com>
2026-02-19 14:30:19 +0000

    Update trip ID character limit validation to 32 characters

--- validator/validations/trips/validations/trip_id_limit_characters_validation.go
+++ validator/validations/trips/validations/trip_id_limit_characters_validation.go
@@ -30,7 +30,7 @@ func TripIdLimitCharactersValidation(trip *types.Trip, row int, gtfs *types.Gtfs
 		return
 	}
 
-	if len(*trip.TripId) > 31 {
+	if len(*trip.TripId) > 32 {
 		ctx.AddError(ctx.GetTranslatedMessage("trip_id_limit_characters_validation.too_long"))
 		return
 	}
