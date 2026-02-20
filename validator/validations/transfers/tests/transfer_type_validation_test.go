d0e22a7009b1c920db7f995704d9834914ed3ceb
LoudNeo <meliodasgamer077@gmail.com>
2026-02-20 17:52:57 +0000

    Implement transfer type validation and update related error messages

---
+++ validator/validations/transfers/tests/transfer_type_validation_test.go
@@ -0,0 +1,28 @@
+package transfers_tests
+
+import (
+	"main/lib/test_helpers"
+	"main/services"
+	"main/types"
+	validations "main/validations/transfers/validations"
+	"testing"
+)
+
+func TestAllTransferTypeValidationTestCases(t *testing.T) {
+	validOptions := []int{0, 1, 2, 3, 4, 5}
+	for _, tc := range test_helpers.GetGenericEnumIntTestCases("transfer_type", validOptions) {
+		t.Run(tc.Name, func(t *testing.T) {
+			services.AppMessageService.Clear()
+
+			var transferType *int
+			if tc.Value != nil {
+				if ptr, ok := tc.Value.(*int); ok {
+					transferType = ptr
+				}
+			}
+
+			validations.TransferTypeValidation(&types.Transfers{TransferType: transferType}, tc.Row, nil)
+			test_helpers.AssertMessageCount(t, services.AppMessageService, tc.ExpectedErrors, tc.Name, types.SEVERITY_ERROR)
+		})
+	}
+}
