fb2850677c527cb27e62b46d0eca877ef697a6c5
LoudNeo <meliodasgamer077@gmail.com>
2026-02-20 16:25:07 +0000

    Add pathways.txt to GTFSFileToTable and GTFSFiles mappings. This update ensures that the pathways file is recognized and processed correctly within the GTFS validation framework.

--- validator/config/gtfs_files.go
+++ validator/config/gtfs_files.go
@@ -18,6 +18,7 @@ var GTFSFileToTable = map[string]string{
 	"periods.txt":         "periods",
 	"shapes.txt":          "shapes",
 	"frequencies.txt":     "frequencies",
+	"pathways.txt":        "pathways",
 }
 
 // GTFSFiles is the set of valid GTFS filenames that will be processed
@@ -39,6 +40,7 @@ var GTFSFiles = map[string]struct{}{
 	"shapes.txt":          {},
 	"vehicles.txt":        {},
 	"frequencies.txt":     {},
+	"pathways.txt":        {},
 }
 
 // GTFSTables is the list of all possible GTFS table names (without .txt extension)
