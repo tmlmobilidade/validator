2e6f6b368c95f7d383a5583f59202a5593ffd117
LoudNeo <meliodasgamer077@gmail.com>
2026-02-20 17:53:22 +0000

    Add min_transfer_time validation logic and corresponding test cases

---
+++ validator/validations/transfers/validations/min_transfer_time_validation.go
@@ -0,0 +1,44 @@
+package transfers
+
+import (
+	"main/lib"
+	"main/services"
+	"main/types"
+)
+
+/*
+# Attributes
+  - File: [transfers.txt]
+  - Field: min_transfer_time
+  - Presence: Optional
+  - Type: Non-negative integer
+
+# Description
+
+Amount of time, in seconds, that must be available to permit a transfer between routes at the specified stops.
+The min_transfer_time should be sufficient to permit a typical rider to move between the two stops, including buffer time to allow for schedule variance on each route.
+
+[transfers.txt]: https://gtfs.org/schedule/reference/#transfertstxt
+*/
+func MinTransferTimeValidation(transfer *types.Transfers, row int, rules *types.TransfersRules) {
+	ctx := lib.NewValidationContext("min_transfer_time", "transfers.txt", "min_transfer_time_validation", row, services.AppMessageService)
+	if rules != nil && rules.MinTransferTime.Severity != "" {
+		ctx.WithSeverity(rules.MinTransferTime.Severity)
+	}
+
+	if transfer.MinTransferTime == nil {
+		if !ctx.ShouldSkip() {
+			ctx.AddMessageWithSeverity(ctx.GetTranslatedMessage("min_transfer_time_validation.required", "min_transfer_time_validation.recommended"))
+		}
+		return
+	}
+
+	if *transfer.MinTransferTime < 0 {
+		ctx.AddError(ctx.GetTranslatedMessage("transfers_validation.invalid", *transfer.MinTransferTime))
+	}
+
+	if ctx.IsForbidden() {
+		ctx.AddMessageWithSeverity(ctx.GetTranslatedMessage("min_transfer_time_validation.forbidden"))
+		return
+	}
+}
