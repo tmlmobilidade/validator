35a61a3bf69662a4040e9be0e4628124bce343fe
LoudNeo <meliodasgamer077@gmail.com>
2026-03-02 16:16:48 +0000

    fix: enhance pattern_id format validation to support multiple formats and improve error messages

--- validator/validations/trips/validations/pattern_id_format_validation.go
+++ validator/validations/trips/validations/pattern_id_format_validation.go
@@ -19,7 +19,12 @@ import (
 Validates if the pattern_id is in the correct format. Must be in the format "X at XXXX_X_X".
 */
 
-var patternIDRegex = regexp.MustCompile(`^[^_]{1,4}_[^_]_[^_]$`)
+const defaultPatternIDFormat = "XXXX_X_X"
+
+var patternIDFormatRegexMap = map[string]*regexp.Regexp{
+	"XXXX_X_X":   regexp.MustCompile(`^[^_]{1,4}_[^_]_[^_]$`),
+	"XXXX_X_XXX": regexp.MustCompile(`^[^_]{1,4}_[^_]_[^_]{3}$`),
+}
 
 func PatternIdFormatValidation(trip *types.Trip, row int, gtfs *types.Gtfs, rules *types.TripsRules) {
 	ctx := lib.NewValidationContext("pattern_id", "trips.txt", "pattern_id_format_validation", row, services.AppMessageService)
@@ -31,8 +36,19 @@ func PatternIdFormatValidation(trip *types.Trip, row int, gtfs *types.Gtfs, rule
 		return
 	}
 
+	expectedFormat := defaultPatternIDFormat
+	patternIDRegex := patternIDFormatRegexMap[defaultPatternIDFormat]
+
+	if rules != nil && rules.PatternIdFormat.Options != nil && len(*rules.PatternIdFormat.Options) > 0 {
+		configuredFormat := (*rules.PatternIdFormat.Options)[0]
+		if configuredRegex, ok := patternIDFormatRegexMap[configuredFormat]; ok {
+			expectedFormat = configuredFormat
+			patternIDRegex = configuredRegex
+		}
+	}
+
 	if !patternIDRegex.MatchString(*trip.PatternId) {
-		ctx.AddError(ctx.GetTranslatedMessage("pattern_id_format_validation.invalid", *trip.PatternId))
+		ctx.AddError(ctx.GetTranslatedMessage("pattern_id_format_validation.invalid", expectedFormat, *trip.PatternId))
 		return
 	}
 }
