2e47ef118f8b1e69f0de84bca47087fcdfab8230
João de Vasconcelos <johnyvasconcelos@icloud.com>
2026-03-04 19:59:05 +0000

    ESLint updates

--- ts-wrapper/eslint.config.mjs
+++ ts-wrapper/eslint.config.mjs
@@ -1,14 +1,9 @@
 /* * */
 
-import { node } from '@carrismetropolitana/eslint'
+import { node } from '@tmlmobilidade/eslint';
 
 /* * */
 
 export default [
-  ...node,
-  {
-    rules: {
-      '@typescript-eslint/no-extraneous-class': 'off',
-    },
-  },
-]
+	...node,
+];
