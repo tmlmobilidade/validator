577a34340954b2c08fe716bfc8356f6605726bc0
LoudNeo <meliodasgamer077@gmail.com>
2026-03-06 11:41:28 +0000

    fix: improve error message for invalid agency timezone validation

--- validator/validations/agency/validations/agency_timezone_validation.go
+++ validator/validations/agency/validations/agency_timezone_validation.go
@@ -24,14 +24,13 @@ If multiple agencies are specified in the dataset, each must have the same 'agen
 */
 func AgencyTimezoneValidation(agency *types.Agency, row int, rules *types.AgencyRules) {
 	ctx := lib.NewValidationContext("agency_timezone", "agency.txt", "agency_timezone_validation", row, services.AppMessageService)
-
 	if agency.AgencyTimezone == nil {
 		ctx.AddError(ctx.GetTranslatedMessage("agency_timezone_validation.required"))
 		return
 	}
 
 	if !lib.ValidateTimezone(*agency.AgencyTimezone) {
-		ctx.AddError(ctx.GetTranslatedMessage("agency_timezone_validation.invalid"))
+		ctx.AddError(ctx.GetTranslatedMessage("agency_timezone_validation.invalid", &agency.AgencyTimezone))
 		return
 	}
 
