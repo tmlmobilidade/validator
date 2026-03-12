822f79d4f0c8e3613f316bce07f89741c3caea1b
LoudNeo <meliodasgamer077@gmail.com>
2026-03-12 14:44:48 +0000

    fix: update license plate format validation

--- validator/validations/vehicles/validations/license_plate_validation.go
+++ validator/validations/vehicles/validations/license_plate_validation.go
@@ -18,7 +18,7 @@ import (
 
 The license plate of the vehicle.
 
-The license plate must be in the format XX-XX-XX.
+The license plate must be in the format XXXXXX.
 */
 func LicensePlateValidation(vehicle *types.Vehicle, row int, gtfs *types.Gtfs, rules *types.VehiclesRules) {
 	ctx := lib.NewValidationContext("license_plate", "vehicles.txt", "license_plate_validation", row, services.AppMessageService)
