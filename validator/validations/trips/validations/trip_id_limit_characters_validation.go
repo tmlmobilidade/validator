7e76af440ae8a5888aa3c464b19ff39f7b549f1a
LoudNeo <meliodasgamer077@gmail.com>
2026-04-23 11:24:55 +0100

    refactor: update trip_id character limit from 32 to 36 and adjust validation context rule ID

--- validator/validations/trips/validations/trip_id_limit_characters_validation.go
+++ validator/validations/trips/validations/trip_id_limit_characters_validation.go
@@ -15,13 +15,13 @@ import (
 
 # Description
 
-Ensures the trip_id is less than or equal to 32 characters.
+Ensures the trip_id is less than or equal to 36 characters.
 
 [trips.txt]: https://gtfs.org/schedule/reference/#trips
 */
 
 func TripIdLimitCharactersValidation(trip *types.Trip, row int, gtfs *types.Gtfs, rules *types.TripsRules) {
-	ctx := lib.NewValidationContext("trip_id_limit_characters", "trips.txt", "trip_id_limit_characters_validation", "trip_id_max_length_and_allowed_charset", row, services.AppMessageService)
+	ctx := lib.NewValidationContext("trip_id_limit_characters", "trips.txt", "trip_id_limit_characters_validation", "trip_id_limit_max_length", row, services.AppMessageService)
 	if rules != nil && rules.TripIdLimitCharacters.Severity != "" {
 		ctx.WithSeverity(rules.TripIdLimitCharacters.Severity)
 	}
