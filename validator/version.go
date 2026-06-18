92436f00668c075f37a5702dd8c02153734ca808
LoudNeo <meliodasgamer077@gmail.com>
2026-06-18 11:26:23 +0100

    chore(validator): obscure Sentry DSN for security reasons

--- validator/version.go
+++ validator/version.go
@@ -4,4 +4,4 @@ package main
 // this are the last version number of the validator
 var version = "20260319.1551.27"
 
-var sentryDSN = "https://b6e10869a96d309777f60890f0b4221f@o4511571034439680.ingest.de.sentry.io/4511571039879248"
+var sentryDSN = "..."
